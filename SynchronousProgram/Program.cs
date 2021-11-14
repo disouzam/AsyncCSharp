3294df1efd9614122c6ac0eb3eb7cbf1d4045a92
Dickson Souza <Dickson.Souza@rhimagnesita.com>
2021-11-14 09:52:34 -0300

    Reducing delays to 15 s instead of 60 s to decrease total execution time to 1 min and 15 s instead of long 5 minutes

--- SynchronousProgram/Program.cs
+++ SynchronousProgram/Program.cs
@@ -74,7 +74,7 @@ namespace SyncBreakfast
           Console.WriteLine("\tPutting a slice of bread in the toaster");
         }
         Console.WriteLine("\tStart toasting...");
-        Task.Delay(60000).Wait();
+        Task.Delay(15000).Wait();
         Console.WriteLine("\tRemove toast from toaster");
       }
 
@@ -87,13 +87,13 @@ namespace SyncBreakfast
       {
         Console.WriteLine($"\tPutting {slices} slices of bacon in the pan");
         Console.WriteLine("\tCooking first side of bacon...");
-        Task.Delay(60000).Wait();
+        Task.Delay(15000).Wait();
         for (int slice = 0; slice < slices; slice++)
         {
           Console.WriteLine("\tFlipping a slice of bacon");
         }
         Console.WriteLine("\tCooking the second side of bacon...");
-        Task.Delay(60000).Wait();
+        Task.Delay(15000).Wait();
         Console.WriteLine("\tPut bacon on plate");
       }
 
@@ -105,10 +105,10 @@ namespace SyncBreakfast
       if (howMany != 0)
       {
         Console.WriteLine("\tWarming the egg pan...");
-        Task.Delay(60000).Wait();
+        Task.Delay(15000).Wait();
         Console.WriteLine($"\tCracking {howMany} eggs");
         Console.WriteLine("\tCooking the eggs ...");
-        Task.Delay(60000).Wait();
+        Task.Delay(15000).Wait();
         Console.WriteLine("\tPut eggs on plate");
       }
 
