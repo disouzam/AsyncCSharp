e6af231692f21f709ea4bfa9d79f26c0a8f4de75
Dickson Souza <Dickson.Souza@rhimagnesita.com>
2021-11-14 18:27:44 -0300

    Creating project Async3

---
+++ Async3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Async3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
