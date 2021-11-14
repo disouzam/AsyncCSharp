4b0b70ff1e2bafe16c3d1bf7f2736c2b5fc73303
Dickson Souza <Dickson.Souza@rhimagnesita.com>
2021-11-14 18:31:45 -0300

    Draft of the code that will be written

--- Async3/Program.cs
+++ Async3/Program.cs
@@ -4,9 +4,15 @@ namespace Async3
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Print a console statement
+            // Call a first async method
+            // Call a sync method that doesn't depend on the first async
+            // Call a second async method 
+            // Call a second sync method that doesn't depend on the first async
+            // Wait for two async methods be complete
+            // Consolidate the result in one single output
         }
     }
 }
