ca9fb4200c6dfda3d0f836bc1938c0e50d64955f
Dickson Souza <Dickson.Souza@rhimagnesita.com>
2021-11-15 10:49:05 -0300

    Adding an event handler to monitor threshold of a counter and configuring the launch tasks in VS Code to run in integrated terminal

--- SixTipsAsync/Program.cs
+++ SixTipsAsync/Program.cs
@@ -2,11 +2,27 @@
 
 namespace SixTipsAsync
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.WriteLine("Tip1: Async void is only for event handlers.");
+
+      Counter c = new Counter(new Random().Next(20));
+      c.ThresholdReached += c_ThresholdReached;
+
+      Console.WriteLine("press 'a' key to increase total");
+      while (Console.ReadKey(true).KeyChar == 'a')
+      {
+        Console.WriteLine("adding one");
+        c.Add(1);
+      }
     }
+
+    static void c_ThresholdReached(object sender, EventArgs e)
+    {
+      Console.WriteLine("The threshold was reached.");
+      Environment.Exit(0);
+    }
+  }
 }
